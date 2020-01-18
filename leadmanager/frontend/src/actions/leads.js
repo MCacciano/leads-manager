a4c4dd14e96a25bb2260234a009e8638b628ec31
mcacciano <michael.cacciano@gmail.com>
2020-01-18 13:59:07 -0500

    added owner field as django User model to Lead model, can no longer see leads if they do not belong to you / are not logged in

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-import { createMessage } from './messages';
+import { createMessage, returnErrors } from './messages';
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
 export const getLeads = () => dispatch => {
   axios
@@ -13,7 +13,11 @@ export const getLeads = () => dispatch => {
         payload: res.data
       })
     )
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(
+        returnErrors({ msg: err.response.data, status: err.response.status })
+      )
+    );
 };
 
 export const deleteLead = id => dispatch => {
@@ -36,15 +40,9 @@ export const addLead = lead => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(
+        returnErrors({ msg: err.response.data, status: err.response.status })
+      )
+    );
 };
--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,6 +1,12 @@
-import { CREATE_MESSAGE } from './types';
+import { CREATE_MESSAGE, GET_ERRORS } from './types';
 
 export const createMessage = message => ({
   type: CREATE_MESSAGE,
   payload: message
+});
+
+// return errors
+export const returnErrors = (msg, status) => ({
+  type: GET_ERRORS,
+  payload: { msg, status }
 });
